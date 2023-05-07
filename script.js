33918ed5a2abe16e722000b44778b522eb6ee965
Shaheli-79 <68340637+Shaheli-79@users.noreply.github.com>
2023-05-08 00:14:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var isLoggedIn=false;
+window.addEventListener('load', function() {
+    if(!JSON.parse(localStorage.getItem("currUser"))){
+        // this.alert("please Login")
+        // window.location.href="./login/login.html";
+    }
+    else{
+    isLoggedIn=true;
+    }
+});
+
+document.getElementById("Home").addEventListener("click",(e)=>{
+    window.location.href="./index.html"
+})
+document.getElementById("Login").addEventListener("click",(e)=>{
+    if(!isLoggedIn)
+    window.location.href="/ShoppingApp/login/login.html"
+    else
+    alert("already logged in !")
+    console.log(localStorage.getItem("currUser"));
+})
+document.getElementById("SignUp").addEventListener("click",(e)=>{
+    if(!isLoggedIn)
+    window.location.href="/ShoppingApp/signup/signup.html"
+    else
+        alert("alredy signed in!")
+})
+document.getElementById("MyCart").addEventListener("click",(e)=>{
+    if(!isLoggedIn)
+    window.location.href="/ShoppingApp/login/login.html"
+    else
+    window.location.href="/ShoppingApp/cart/cart.html"
+})
+document.getElementById("Profile").addEventListener("click",(e)=>{
+    if(!isLoggedIn)
+    window.location.href="/ShoppingApp/login/login.html"
+    else
+    window.location.href="/ShoppingApp/profile/profile.html";
+})
+document.getElementById("login").addEventListener("click",(e)=>{
+    if(!isLoggedIn)
+    window.location.href="/ShoppingApp/login/login.html"
+    else
+    window.location.href="/ShoppingApp/profile/profile.html";
+})
+
+document.getElementById("signup").addEventListener("click",(e)=>{
+    if(!isLoggedIn)
+    window.location.href="/ShoppingApp/signup/signup.html"
+    else{
+        alert("already signed in !")
+    window.location.href="/ShoppingApp/profile/profile.html";
+    }
+})
+
+  
